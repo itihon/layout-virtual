55faa80cae42f977f165252a6e0b59503da81ae3
itihon <kalabin.alexandr@gmail.com>
2026-05-08 20:05:02 +0500

    chore: add vite fixtures build config for testing react

--- vite.tests.config.js
+++ vite.tests.config.js
@@ -1,15 +1,46 @@
 import { defineConfig } from 'vite';
 
+const fixture = (path) => new URL(`./tests/fixtures/${path}`, import.meta.url).pathname;
+const src = (path) => new URL(`./src/${path}`, import.meta.url).pathname;
+
 export default defineConfig(({ command, mode, isSsrBuild, isPreview }) => {
-  return {
-    css: {
-      modules: {
-        generateScopedName: '[local]',
+  if (mode === 'development') {
+    return {
+      css: {
+        modules: {
+          generateScopedName: '[local]',
+        },
       },
-    },
-    root: './tests/fixtures/',
-    server: {
-      host: '0.0.0.0',
-    },
-  };
+      root: './tests/fixtures/',
+      server: {
+        host: '0.0.0.0',
+      },
+    };
+  }
+  else {
+    return {
+      // build specific config
+      root: './tests/fixtures/',
+      resolve: {
+        alias: {
+          './DOMConstructor': src('Renderer/DOMConstructor.ts'),
+        },
+      },
+      build: {
+        outDir: '../../dist/fixtures',
+        copyPublicDir: false,
+        rollupOptions: {
+          input: {
+            index: fixture('index.html'),
+            scrollableContainer: fixture('scrollable-container.html'),
+            fixedListLayout: fixture('VirtualizedList/FixedListLayout/index.html'),
+            dynamicListLayout: fixture('VirtualizedList/DynamicListLayout/index.html'),
+            unknownScrollHeight: fixture('VirtualizedList/DynamicListLayout/unknown-scroll-height.html'),
+            react: fixture('VirtualizedList/React/index.html'),
+          },
+        },
+        emptyOutDir: true,
+      },
+    };
+  }
 });
