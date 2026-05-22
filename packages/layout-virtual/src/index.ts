ba9a96eaaa5bcccdc8bced83944fdbfdbd46ef41
itihon <kalabin.alexandr@gmail.com>
2026-05-22 11:26:44 +0500

    refactor(layout-virtual): add DOMRenderer missing export

--- packages/layout-virtual/src/index.ts
+++ packages/layout-virtual/src/index.ts
@@ -8,3 +8,4 @@ export { default } from './LayoutVirtual';
 export { default as ScrollableContainer } from './Renderer/NativeScrollContainer';
 export { default as DynamicListLayout } from './Layout/DynamicListLayout';
 export { default as ArrayItemStore } from './ItemStore/ArrayItemStore';
+export { default as DOMRenderer } from './Renderer/DOMRenderer';
