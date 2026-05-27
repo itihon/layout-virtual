bac3418c3b38ef5e16917e9461c5e3145377ff15
itihon <kalabin.alexandr@gmail.com>
2026-05-27 12:18:24 +0500

    chore: add mangled names caching in terser config to make naming consistent across chunks

--- vite.package.config.js
+++ vite.package.config.js
@@ -23,6 +23,7 @@ export function definePackageConfig({ packageDir, plugins = [], external = [], e
             regex: /^_/,
           },
         },
+        nameCache: {},
       },
     },
     css: {
