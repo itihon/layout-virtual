fe913c2734449e0ba43a913d04eaddf4b2cc6674
itihon <kalabin.alexandr@gmail.com>
2026-06-07 23:09:58 +0500

    refactor(homepage): improve the example on the landing page

--- homepage/src/components/ResponsiveGridExample.tsx
+++ homepage/src/components/ResponsiveGridExample.tsx
@@ -1,9 +1,7 @@
-import React from 'react';
-import VirtualizedGrid, { type VirtualizedListReactClasses, type ListItemProps } from 'react-layout-virtual';
 import Resizer from './Resizer';
+import VirtualizedGrid, { type VirtualizedListReactClasses, type ListItemProps } from 'react-layout-virtual';
 import classes from './ResponsiveGridExample.module.css';
 
-const itemsCount = 1000;
 const styling: VirtualizedListReactClasses = {
   scrollerClass: classes['lv-scroller'],
   viewportClass: classes['lv-viewport'],
@@ -12,35 +10,39 @@ const styling: VirtualizedListReactClasses = {
 
 type Data = { i: number };
 
+const headers = [
+  'One line header.', 
+  'Longer header, wraps to the next lines.',
+];
+
+const descriptions = [
+  'A very short description.',
+  'A much longer description text that wraps to the next lines at different card sizes.',
+];
+
 function ListItem({ data, ref, index }: ListItemProps<Data>) {
   const i = data.i;
 
-  const extraLines =
-    i % 5 === 0 ? ['Second line.', 'Third line.', 'Fourth line.'] :
-    i % 3 === 0 ? ['Second line.', 'Third line.'] :
-    i % 2 === 0 ? ['Second line.'] :
-    [];
+  const header = i % 2 ? headers[0] : headers[1];
+  const description = i % 2 ? descriptions[0] : descriptions[1];
 
   return (
     <div ref={ref} className={classes['list-item']} id={`item-${i}`} data-index={index}>
-      <div className={classes.icon}>📁</div>
-      {`Item ${i}.`}
-      {extraLines.map((text, idx) => <div key={idx}>{text}</div>)}
+      <div className={classes['li-icon']}>📁</div>
+      <div className={classes['li-index']}>{`#${i}`}</div>
+      <div className={classes['li-header']}>{ header }</div>
+      <div className={classes['li-description']}>{ description }</div>
     </div>
   );
 };
 
-interface Props {
-  
-}
-
-const ResponsiveGridExample: React.FC<Props> = ({  }) => {
-  const data = Array.from({ length: itemsCount }, (_, i) => ({ i }));
+const ResponsiveGridExample = () => {
+  const data = Array.from({ length: 1000 }, (_, i) => ({ i }));
 
   return (
     <>
       <div>Rendered indeces: n - n of n.</div>
-      <Resizer className={classes.resizer} buttonRight={18} buttonBottom={4} initialWidth={'90%'} initialHeight={'70vh'} widthFactor={2}>
+      <Resizer className={classes.resizer} buttonRight={18} buttonBottom={4} initialWidth={'100%'} initialHeight={'70vh'} widthFactor={2}>
         <VirtualizedGrid<Data> overscanHeight={100} data={data} renderItem={ListItem} {...styling} />
       </Resizer>
     </>
