c97947854b34457c01ad1a07fc30f0c19a9ca2ff
itihon <kalabin.alexandr@gmail.com>
2026-07-12 13:12:20 +0500

    fix(homepage): correct import version for angular

--- homepage/src/examples/infinite-scroll/angular/imports.ts
+++ homepage/src/examples/infinite-scroll/angular/imports.ts
@@ -1,8 +1,8 @@
 const imports = {
-  "@angular/core": "https://esm.sh/@angular/core@21.2.17/es2022/core.mjs",
-  "@angular/common": "https://esm.sh/@angular/common@21.2.17/es2022/common.mjs",
-  "@angular/compiler": "https://esm.sh/@angular/compiler@21.2.17/es2022/compiler.mjs",
-  "@angular/platform-browser": "https://esm.sh/@angular/platform-browser@21.2.17/es2022/platform-browser.mjs",
+  "@angular/core": "https://esm.sh/@angular/core@21.2.18/es2022/core.mjs",
+  "@angular/common": "https://esm.sh/@angular/common@21.2.18/es2022/common.mjs",
+  "@angular/compiler": "https://esm.sh/@angular/compiler@21.2.18/es2022/compiler.mjs",
+  "@angular/platform-browser": "https://esm.sh/@angular/platform-browser@21.2.18/es2022/platform-browser.mjs",
 };
 
 export default imports;
